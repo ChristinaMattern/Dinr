70cf15ada45c23e5558c048d4762ae425c17e504
SmithNola <35985021+SmithNola@users.noreply.github.com>
2019-04-07 23:14:24 -0400

    Add files via upload

--- Java/Register.java
+++ Java/Register.java
@@ -4,16 +4,20 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.internal.measurement.zzaa;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.ProviderQueryResult;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -57,80 +61,102 @@ public class Register extends AppCompatActivity {
                 final String password = newUserPassword.getText().toString();
                 final String confirmPass = confirmPassword.getText().toString();
                 final String id = studentID.getText().toString();
-                final boolean[] checked = new boolean[1];//to approve that id was checked
-                checked[0] = true;
-                mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
-
-                mDatabase.addListenerForSingleValueEvent(new ValueEventListener(){
+                final boolean[] checked = new boolean[2];//to approve that id was checked
+                checked[0] = true;//id slot
+                checked[1]=false ;
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                mAuth.fetchProvidersForEmail(email).addOnCompleteListener(new OnCompleteListener<ProviderQueryResult>() {
                     @Override
-                    public void onDataChange(@NonNull DataSnapshot snapShot) {
-                        if(snapShot.hasChild(id)){//checks to see if another user has an the same id
-                            checked[0]=false;
+                    public void onComplete(@NonNull Task<ProviderQueryResult> task) {
+                        if(task.isSuccessful()){//if email is not in use then it will register
+                            checked[1]=true;
                         }
-                            if (checked[0] == true) {
-                                if (validate(fName, lName, id, email, password, confirmPass)) {
+                        if(checked[1]==true){
 
-                                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<AuthResult> task) {
-                                            if (task.isSuccessful()) {
-                                                mDatabase = FirebaseDatabase.getInstance().getReference();
-                                                writeNewUser(id, fName, lName, email, password);
+                            mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+                            mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapShot) {
+                                    if (snapShot.hasChild(id)) {//checks to see if another user has an the same id
+                                        checked[0] = false;
+                                    }
+                                    if (checked[0] == true) {
+                                        if (validate(fName, lName, id, email, password, confirmPass)) {
 
-                                                Toast.makeText(Register.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
-                                                startActivity(new Intent(Register.this, HomeScreen.class));
-                                            } else {
-                                                Toast.makeText(Register.this, "Registration Unsuccessful!", Toast.LENGTH_SHORT).show();
-                                            }
+                                            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                                    if (task.isSuccessful()) {
+                                                        mDatabase = FirebaseDatabase.getInstance().getReference();
+                                                        writeNewUser(id, fName, lName, email, password);
+                                                        Toast.makeText(Register.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
+                                                        startActivity(new Intent(Register.this, HomeScreen.class));
+                                                    } else {
+                                                        try
+                                                        {
+                                                            throw task.getException();
+                                                        }
+                                                        catch (FirebaseAuthUserCollisionException existEmail)
+                                                        {
+                                                            Toast.makeText(Register.this, "Email already registered", Toast.LENGTH_SHORT).show();
+
+
+                                                        }
+                                                        catch (Exception e)
+                                                        {
+                                                            Toast.makeText(Register.this, "Registration Unsuccessful!", Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                                                }
+                                            });
                                         }
-                                    });
+                                    } else {
+                                        Toast.makeText(Register.this, "ID is already registered", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
 
                                 }
-                            } else {
-                                Toast.makeText(Register.this, "ID is already registered", Toast.LENGTH_SHORT).show();
-                            }
+                            });
 
-
-
+                        }
+                        else{
+                            Toast.makeText(Register.this, "Email is already registered", Toast.LENGTH_SHORT).show();
+                        }
                     }
-
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-
                 });
-
             }
         });
     }
 
-            //adds user to database
-            private void writeNewUser(String id, String fName, String lName, String email, String password) {
-                User user = new User(id, fName, lName, email, password);
+    //adds user to database
+    private void writeNewUser(String id, String fName, String lName, String email, String password) {
+        User user = new User(id, fName, lName, email, password);
 
-                mDatabase.child("users").child(id).setValue(id);
-                mDatabase.child("users").child(id).child("fName").setValue(fName);
-                mDatabase.child("users").child(id).child("lName").setValue(lName);
-                mDatabase.child("users").child(id).child("email").setValue(email);
-                mDatabase.child("users").child(id).child("password").setValue(password);
+        mDatabase.child("users").child(id).setValue(id);
+        mDatabase.child("users").child(id).child("fName").setValue(fName);
+        mDatabase.child("users").child(id).child("lName").setValue(lName);
+        mDatabase.child("users").child(id).child("email").setValue(email);
+        mDatabase.child("users").child(id).child("password").setValue(password);
 
-            }
+    }
 
 
-            private Boolean validate(String fName, String lName, final String id, String email, String password, String confirmPass) {
-                if (!fName.isEmpty() && !lName.isEmpty() && !id.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmPass.isEmpty() && password.equals(confirmPass) && password.length() >= 6) {
-                    return true;
-                } else if (password.length() < 6) {
-                    Toast.makeText(Register.this, "Passwords must be 6 numbers or longer!", Toast.LENGTH_SHORT).show();
-                } else if (!password.equals(confirmPass)) {
-                    Toast.makeText(Register.this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(Register.this, "All fields must be filled!", Toast.LENGTH_SHORT).show();
-                    ;
-                }
-                return false;
-            }
+    private Boolean validate(String fName, String lName, final String id, String email, String password, String confirmPass) {
+        if (!fName.isEmpty() && !lName.isEmpty() && !id.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmPass.isEmpty() && password.equals(confirmPass) && password.length() >= 6) {
+            return true;
+        } else if (password.length() < 6) {
+            Toast.makeText(Register.this, "Passwords must be 6 numbers or longer!", Toast.LENGTH_SHORT).show();
+        } else if (!password.equals(confirmPass)) {
+            Toast.makeText(Register.this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(Register.this, "All fields must be filled!", Toast.LENGTH_SHORT).show();
+            ;
+        }
+        return false;
+    }
 
 }
+
