7d25840bfd5863f61cfdb0bff4fd9940a0831908
SmithNola <35985021+SmithNola@users.noreply.github.com>
2019-04-09 02:21:12 -0400

    Add files via upload

--- Java/MyProfile.java
+++ Java/MyProfile.java
@@ -1,15 +1,58 @@
 package com.example.dinr;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class MyProfile extends AppCompatActivity {
-
+    TextView fullName;
+    private String userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_profile);
+        fullName = (TextView)findViewById(R.id.fullName);
+        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+        userId=currentFirebaseUser.getUid();
+        final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        Query query = rootRef.child("users").orderByChild("userId").equalTo(userId);
 
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String fName="";
+                String lName="";
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                     fName= (String) ds.child("fName").getValue();
+                     lName= (String) ds.child("lName").getValue();
+                }
+
+                fullName.setText(fName+" "+lName);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        query.addListenerForSingleValueEvent(valueEventListener);
 
     }
 
