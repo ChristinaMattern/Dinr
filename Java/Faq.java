ae611ab0f204f873997c97dd1077b1fe72500ae3
AngelaCebada <46796358+AngelaCebada@users.noreply.github.com>
2019-04-18 13:46:55 -0400

    Update Faq.java

--- Java/Faq.java
+++ Java/Faq.java
@@ -1,9 +1,19 @@
+
 package com.example.dinr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Faq extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +21,40 @@ public class Faq extends AppCompatActivity {
         setContentView(R.layout.faq);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_screen_menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        firebaseAuth = firebaseAuth.getInstance();
+        switch (item.getItemId()){
+            case R.id.Settings:
+                startActivity(new Intent(Faq.this, Settings.class));
+                return true;
+            case R.id.Logout:
+                Toast.makeText(Faq.this, "Logging Out...", Toast.LENGTH_SHORT).show();
+                firebaseAuth.signOut();
+                startActivity(new Intent(Faq.this, LoginScreen.class));
+                return true;
+            case R.id.Home:
+                startActivity(new Intent(Faq.this, HomeScreen.class));
+                return true;
+            case R.id.MyProfile:
+                startActivity(new Intent(Faq.this, MyProfile.class));
+                return true;
+            case R.id.EditProfile:
+                startActivity(new Intent(Faq.this, EditProfile.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
