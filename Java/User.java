e548578e4546cace15f71b01d6b72e9261d6ee37
SmithNola <35985021+SmithNola@users.noreply.github.com>
2019-04-26 23:50:54 -0400

    Add files via upload

--- Java/User.java
+++ Java/User.java
@@ -1,25 +1,71 @@
 package com.example.dinr;
+/*@author Nola Smtih
+@date 4/25/2019 */
+public class User {
 
-    public class User {
-        private String id;
-        private String fName;
-        private String lName ;
-        private String email;
-        private String password;
+    public String name;
+    public String major;
+    public String location;
+    public String year;
+    public String otherID;
 
-        public User() {
-
-        }
-
-        public User(String id,String fName, String lName, String email, String password) {
-            this.fName=fName;
-            this.lName=lName;
-            this.email=email;
-            this.password=password;
-            this.id = id;
-        }
-
-
-
+    public User(){
 
     }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+    public String getMajor() {
+
+        return major;
+    }
+
+    public void setMajor(String Major) {
+
+        this.major = major;
+    }
+    public String getLocation() {
+
+        return location;
+    }
+
+    public void setLocation(String location) {
+
+        this.location = location;
+    }
+    public String getYear() {
+
+        return year;
+    }
+
+    public void setYear(String year) {
+
+        this.year = year;
+    }
+
+    public String getOtherID() {
+
+        return otherID;
+    }
+
+    public void setOtherID(String otherID) {
+
+        this.otherID= otherID;
+    }
+
+    public User(String name,String major, String location, String year, String otherID) {
+        this.name = name;
+        this.year=year;
+        this.location=location;
+        this.major=major;
+        this.otherID=otherID;
+
+    }
+}
