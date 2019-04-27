858575c803220dde381934c7ca9879aad6ff51f9
SmithNola <35985021+SmithNola@users.noreply.github.com>
2019-04-27 02:38:22 -0400

    Add files via upload

---
+++ Java/OtherProfile.java
@@ -0,0 +1,126 @@
+package com.example.dinr;
+/*@author Nola Smtih
+@date 4/17/2019 */
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class OtherProfile extends AppCompatActivity {
+    private TextView fullName;
+    private String userId;
+    private TextView bioText;
+    private TextView yearText;
+    private TextView majorText;
+    private Button editButton;
+    private ImageView userPic;
+    private TextView locationText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.other_profile);
+        fullName = (TextView)findViewById(R.id.fullName);
+        bioText = (TextView)findViewById(R.id.bioText);
+        yearText = (TextView)findViewById(R.id.yearText);
+        majorText = (TextView)findViewById(R.id.majorText);
+        editButton = (Button)findViewById(R.id.edit_button);
+        locationText = (TextView) findViewById(R.id.locationText);
+        SharedPreferences sharedPref=getSharedPreferences("OtherId", Context.MODE_PRIVATE);
+        userId=sharedPref.getString("id","");
+        final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        Query query = rootRef.child("users").orderByChild("userId").equalTo(userId);//finds the user in the database
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String fName=" ";
+                String lName=" ";
+                String bio=" ";
+                String year=" ";
+                String major=" ";
+                String location=" ";
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String id = (String) ds.getKey();//retrieves user's id to know where to put info into profile
+                    fName= (String) ds.child("fName").getValue();//retrieves first name
+                    lName= (String) ds.child("lName").getValue();//retrieves last name
+                    bio= (String) ds.child("bio").getValue();//retrieves bio
+                    year= (String) ds.child("year").getValue();//retrieves year
+                    major= (String) ds.child("major").getValue();//retrieves major
+                    location=(String) ds.child("location").getValue();
+                    StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+                    StorageReference photoReference= storageReference.child(id).child("image");
+                    userPic = (ImageView)findViewById(R.id.userPic);
+                    final long ONE_MEGABYTE = 1024 * 1024;
+                    photoReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                        @Override
+                        public void onSuccess(byte[] bytes) {
+                            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                            userPic.setImageBitmap(bmp);
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            Toast.makeText(getApplicationContext(), "No Such file or Path found!!", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+
+                fullName.setText(fName+" "+lName);//combines first and last name to create full name
+                bioText.setText(bio);//sets bio
+                yearText.setText(year);//sets year
+                majorText.setText(major);//sets major
+                locationText.setText(location);//sets location
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        query.addListenerForSingleValueEvent(valueEventListener);
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OtherProfile.this, EditProfile.class));
+            }
+        });
+
+    }
+}
