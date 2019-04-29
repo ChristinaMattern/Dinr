3bfba1aec9e8ae943004c7e783b29808aca5907c
AngelaCebada <46796358+AngelaCebada@users.noreply.github.com>
2019-04-29 17:30:22 -0400

    Update Settings.java

--- Java/Settings.java
+++ Java/Settings.java
@@ -1,5 +1,12 @@
 package com.example.dinr;
 
+/**
+ * @author Angela Cebada
+ * @date 04/25/2019
+ * @updated 04/29/2019
+ * This is the Settings page
+ */
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,80 +14,41 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
-import android.widget.Spinner;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.firebase.auth.FirebaseAuth;
 
-import org.w3c.dom.Text;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Settings extends AppCompatActivity {
 
-import static java.lang.System.in;
-
-public class Settings extends AppCompatActivity implements AdapterView.OnItemSelectedListener, CompoundButton.OnCheckedChangeListener {
-
-    Spinner spinnermenu;
-    TextView textView;
-    Switch loginSwitch;
     private FirebaseAuth firebaseAuth;
+    TextView textView1;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
-        spinnermenu = (Spinner) findViewById(R.id.spinner);
+        textView1 = (TextView) findViewById(R.id.changePasswordTV);
+        textView1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Settings.this, ChangePassword.class);
+                startActivity(intent);
+            }
+        });
 
-        List<String> fontList = new ArrayList<>();
-        fontList.add("Default");
-        fontList.add("Small");
-        fontList.add("Large");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, fontList);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnermenu.setAdapter(adapter);
-        spinnermenu.setOnItemSelectedListener(this);
-
-        textView = (TextView) findViewById(R.id.profileTV);
-        textView.setOnClickListener(new View.OnClickListener(){
+        textView2 = (TextView) findViewById(R.id.profileTV);
+        textView2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(Settings.this, EditProfile.class);
                 startActivity(intent);
             }
         });
-
-        loginSwitch = (Switch) findViewById(R.id.loginSwitch);  //initiate switch
-        loginSwitch.setOnCheckedChangeListener(this);
-
-
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (isChecked) {
-
-            }
-        else {
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -116,4 +84,5 @@ public class Settings extends AppCompatActivity implements AdapterView.OnItemSel
         }
     }
 }
+
 
