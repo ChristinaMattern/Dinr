c593b3e2f5adf74402b5bf400bc4f2877657365e
ChristinaMattern <46796474+ChristinaMattern@users.noreply.github.com>
2019-04-28 21:41:17 -0400

    Add files via upload

--- Java/LoginScreen.java
+++ Java/LoginScreen.java
@@ -41,48 +41,54 @@ public class LoginScreen extends AppCompatActivity {
         registerButton = findViewById(R.id.registerButton);
         forgotPassBtn = findViewById(R.id.forgotPassButton);
 
+
         firebaseAuth = FirebaseAuth.getInstance();
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            // creates an OnClick that brings to validate method
-            @Override
-            public void onClick(View v) {
-                validate();
-            }
-
-            public void validate() {
-                String email = userNameEditText.getText().toString();
-                String password = passwordEditTextNum.getText().toString();
-                if(!email.isEmpty() && !password.isEmpty()){
-                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                                Toast.makeText(LoginScreen.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(LoginScreen.this, HomeScreen.class));
-                            }
-                            else{
-                                Toast.makeText(LoginScreen.this, "Login Unsuccessful!", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
+        if (firebaseAuth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginScreen.this, HomeScreen.class));
+            finish();
+        } else {
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                // creates an OnClick that brings to validate method
+                @Override
+                public void onClick(View v) {
+                    validate();
                 }
-                else{
-                    Toast.makeText(LoginScreen.this, "All fields must be filled!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginScreen.this, Register.class));
-            }
-        });
-        forgotPassBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginScreen.this, ForgotPass.class));
-            }
-        });
+                public void validate() {
+                    String email = userNameEditText.getText().toString();
+                    String password = passwordEditTextNum.getText().toString();
+                    if (!email.isEmpty() && !password.isEmpty()) {
+                        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginScreen.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(LoginScreen.this, HomeScreen.class));
+                                } else {
+                                    Toast.makeText(LoginScreen.this, "Login Unsuccessful!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    } else {
+                        Toast.makeText(LoginScreen.this, "All fields must be filled!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            registerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(LoginScreen.this, Register.class));
+                }
+            });
+            forgotPassBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(LoginScreen.this, ForgotPass.class));
+                }
+            });
+        }
     }
+    @Override
+    public void onBackPressed(){}
 }
