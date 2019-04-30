3415a64b715ea8db7ccf6b59ce2615be630f9942
SmithNola <35985021+SmithNola@users.noreply.github.com>
2019-04-29 23:45:49 -0400

    Add files via upload

--- Java/OtherProfile.java
+++ Java/OtherProfile.java
@@ -10,24 +10,13 @@ import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -45,7 +34,7 @@ public class OtherProfile extends AppCompatActivity {
     private TextView majorText;
     private ImageView userPic;
     private TextView locationText;
-    private FirebaseAuth firebaseAuth;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +45,7 @@ public class OtherProfile extends AppCompatActivity {
         yearText = (TextView)findViewById(R.id.yearText);
         majorText = (TextView)findViewById(R.id.majorText);
         locationText=(TextView)findViewById(R.id.location);
+        backButton=(Button)findViewById(R.id.back);
 
         SharedPreferences sharedPref=getSharedPreferences("OtherId", Context.MODE_PRIVATE);
         userId=sharedPref.getString("id","");
@@ -112,34 +102,12 @@ public class OtherProfile extends AppCompatActivity {
             }
         };
         query.addListenerForSingleValueEvent(valueEventListener);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OtherProfile.this, FriendSearch.class));
+            }
+        });
 
-
-    }
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.other_profile_menu, menu);
-        return true;
-
-    }
-    public boolean onOptionsItemSelected(MenuItem item) {
-        firebaseAuth = firebaseAuth.getInstance();
-        switch (item.getItemId()) {
-            case R.id.Settings:
-                startActivity(new Intent(OtherProfile.this, Settings.class));
-                return true;
-            case R.id.Logout:
-                Toast.makeText(OtherProfile.this, "Logging Out...", Toast.LENGTH_SHORT).show();
-                firebaseAuth.signOut();
-                startActivity(new Intent(OtherProfile.this, LoginScreen.class));
-                return true;
-            case R.id.Help:
-                startActivity(new Intent(OtherProfile.this, Faq.class));
-                return true;
-            case R.id.Home:
-                startActivity(new Intent(OtherProfile.this, HomeScreen.class));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 }
