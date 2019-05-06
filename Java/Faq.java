fbbe64e3111ef346ed5d2c2e2ea1123679a10d37
AngelaCebada <46796358+AngelaCebada@users.noreply.github.com>
2019-05-06 18:11:28 -0400

    Update Faq.java

--- Java/Faq.java
+++ Java/Faq.java
@@ -1,4 +1,3 @@
-
 package com.example.dinr;
 
 import android.content.Intent;
@@ -10,17 +9,46 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.ms.square.android.expandabletextview.ExpandableTextView;
 
 public class Faq extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    ExpandableTextView expandableTextView1;
+    ExpandableTextView expandableTextView2;
+    ExpandableTextView expandableTextView3;
+    ExpandableTextView expandableTextView4;
+    ExpandableTextView expandableTextView5;
+    ExpandableTextView expandableTextView6;
+    ExpandableTextView expandableTextView7;
+
+    String quest1 ="How do I change my profile picture?" + "\n\n" + "Go to edit your profile and click on the picture and you will be asked to select a photo from your library.";
+    String quest2 ="How do I let others know I am currently at a dining location?" + "\n\n" + "On your profile beneath your name there is a location list where you can choose where you are currently located or if you are offline, so when others click on your profile they will see your location.";
+    String quest3 ="How do I change my password?" + "\n\n" + "You can change your password by going into Settings and clicking on 'Change Password'";
+    String quest4 ="How do I search for friends?" + "\n\n" + "You can access friend search on the home page of the app. Once you click on 'Friend Search' you will have the option to search for your friends by name, where you can access their profile, add or remove them as friends, and view their location and duration.";
+    String quest5 ="Why can't I find my friend?" + "\n\n" + "You may not be able to find someone because their profile might be set to private or they may not have created an account yet.";
+    String quest6 ="Do I need to sign in on the Dining pages?" + "\n\n" + "No, these pages are run by The College of Saint Rose. You do not need to sign in to view the web page.";
+    String quest7 ="Can't find your answer?" + "\n\n" + "Email us at info@dinr.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.faq);
 
-
+        expandableTextView1 = (ExpandableTextView) findViewById(R.id.expandable_text_view1);
+        expandableTextView1.setText(quest1);
+        expandableTextView2 = (ExpandableTextView) findViewById(R.id.expandable_text_view2);
+        expandableTextView2.setText(quest2);
+        expandableTextView3 = (ExpandableTextView) findViewById(R.id.expandable_text_view3);
+        expandableTextView3.setText(quest3);
+        expandableTextView4 = (ExpandableTextView) findViewById(R.id.expandable_text_view4);
+        expandableTextView4.setText(quest4);
+        expandableTextView5 = (ExpandableTextView) findViewById(R.id.expandable_text_view5);
+        expandableTextView5.setText(quest5);
+        expandableTextView6 = (ExpandableTextView) findViewById(R.id.expandable_text_view6);
+        expandableTextView6.setText(quest6);
+        expandableTextView7 = (ExpandableTextView) findViewById(R.id.expandable_text_view7);
+        expandableTextView7.setText(quest7);
     }
 
     @Override
